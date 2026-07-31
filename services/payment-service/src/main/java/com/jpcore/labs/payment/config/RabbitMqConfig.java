391d75894e488cfe9ed2b244324e90f803b7382c
juan.eugenio <juan.eugenio@elogroup.com.br>
2026-07-30 21:58:56 -0300

    feat: Add the producer-side RabbitMQ topology in payment-service

--- services/payment-service/src/main/java/com/jpcore/labs/payment/config/RabbitMqConfig.java
+++ services/payment-service/src/main/java/com/jpcore/labs/payment/config/RabbitMqConfig.java
@@ -1,5 +1,10 @@
 package com.jpcore.labs.payment.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -9,6 +14,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
+
+    public static final String PAYMENT_EXCHANGE = "payment.exchange";
+    public static final String PAYMENT_PROCESS_QUEUE = "payment.process.queue";
+    public static final String PAYMENT_PROCESS_ROUTING_KEY = "payment.process";
+
+    @Bean
+    public DirectExchange paymentExchange() {
+        return new DirectExchange(PAYMENT_EXCHANGE, true, false);
+    }
+
+    @Bean
+    public Queue paymentProcessQueue() {
+        return QueueBuilder.durable(PAYMENT_PROCESS_QUEUE).build();
+    }
+
+    @Bean
+    public Binding paymentProcessBinding(Queue paymentProcessQueue, DirectExchange paymentExchange) {
+        return BindingBuilder.bind(paymentProcessQueue)
+                .to(paymentExchange)
+                .with(PAYMENT_PROCESS_ROUTING_KEY);
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
